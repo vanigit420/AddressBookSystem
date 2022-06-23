374cad3f3257d644dddf4e48e7bafa9502f03561
shivani korwan <shivanikorwan63232@gmail.com>
2022-06-23 08:44:56 +0530

    Adding new contact to address book

--- AddressBook/AddressBook/AddressBook.cs
+++ AddressBook/AddressBook/AddressBook.cs
@@ -20,12 +20,34 @@ namespace AddressBook
         public string Email { get; set; }
 
         //Method to display Contact.
-        public void displayContact()
+        public void DisplayContact()
         {
             Console.WriteLine("FirstName: " + this.FirstName + " LastName: " + this.LastName
                             + " Address: " + this.Address + " City: " + this.City + " State: "
                             + this.State + " Zip: " + this.Zip + " PhoneNumber: "
                             + this.PhoneNumber + " Email: " + this.Email);
+        }
+
+        //Method to Add Contact in a List.
+        public void AddContact()
+        {
+            Console.Write("Enter First Name, Last Name, Address, City, State, Zip, Phone Number, Email \n");
+            AddressBook addressBook = new AddressBook()  //Initializing elements using collection-initializer 
+            {
+                FirstName = Console.ReadLine(),
+                LastName = Console.ReadLine(),
+                Address = Console.ReadLine(),
+                City = Console.ReadLine(),
+                State = Console.ReadLine(),
+                Zip = Convert.ToDouble(Console.ReadLine()),
+                PhoneNumber = Convert.ToDouble(Console.ReadLine()),
+                Email = Console.ReadLine(),
+            };
+
+            //creating a List to store contacts in List.
+            IList<AddressBook> AddreddBookList = new List<AddressBook>();  //created List of class Type.
+            AddreddBookList.Add(addressBook);
+            addressBook.DisplayContact();
         }
     }
 }
